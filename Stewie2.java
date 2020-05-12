6145f616d509e96bccc88843394bb77bf621e1a3
baumsarah <48698640+baumsarah@users.noreply.github.com>
2020-05-12 01:53:49 -0400

    Update Stewie2.java

--- Stewie2.java
+++ Stewie2.java
@@ -1,4 +1,5 @@
-/* Write a complete Java program in a class named Stewie2 that prints the following output. Use at least one static method besides main to remove redundancy.
+/* Write a complete Java program in a class named Stewie2 that prints the following output. 
+Use at least one static method besides main to remove redundancy.
 //////////////////////
 || Victory is mine! ||
 \\\\\\\\\\\\\\\\\\\\\\
