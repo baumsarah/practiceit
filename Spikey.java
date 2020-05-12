0d196ff944c2f5cebd2033546901000523dc56c6
baumsarah <48698640+baumsarah@users.noreply.github.com>
2020-05-11 23:58:02 -0400

    Create Spikey.java
    Completed Exercise 1.2!

---
+++ Spikey.java
@@ -0,0 +1,44 @@
+/*
+* Write a complete Java program in a class named Spikey that prints the following output: 
+*  \/
+* \\//
+*\\\///
+*///\\\
+* //\\
+*  /\
+*/
+
+public class Spikey {
+    public static void main(String[] args) {
+        top();
+        bottom();
+    }
+    public static void top() {
+        for(int i = 0; i < 3; i++) {
+            for(int j = 2 - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j <= i; j++) {
+                System.out.print("\\");
+            }
+            for(int j = 0; j <= i; j++) {
+                System.out.print("/");
+            }
+            System.out.println();
+        }
+    }
+    public static void bottom() {
+        for(int i = 3; i > 0; i--) {
+            for(int j = 0; j < 3 - i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < i; j++) {
+                System.out.print("/");
+            }
+            for(int j = 0; j < i; j++) {
+                System.out.print("\\");
+            }
+            System.out.println();
+        }
+    }
+}
