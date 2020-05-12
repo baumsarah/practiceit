5d9db25fd08b7afac9f4dafe6d99be7e6de45e2c
baumsarah <48698640+baumsarah@users.noreply.github.com>
2020-05-11 23:59:41 -0400

    Update Stewie.java

--- Stewie.java
+++ Stewie.java
@@ -1,3 +1,9 @@
+/* Write a complete Java program in a class named Stewie that prints the following output:
+//////////////////////
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+*/
+
 public class Stewie {
     public static void main(String[] args) {
         print("/");
