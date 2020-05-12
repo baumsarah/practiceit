ce662736bd6bb3cbe76a858a7bd640c9e10feca8
baumsarah <48698640+baumsarah@users.noreply.github.com>
2020-05-11 23:58:48 -0400

    Update Spikey.java

--- Spikey.java
+++ Spikey.java
@@ -1,11 +1,11 @@
 /*
 * Write a complete Java program in a class named Spikey that prints the following output: 
-*  \/
-* \\//
-*\\\///
-*///\\\
-* //\\
-*  /\
+*   \/
+*  \\//
+* \\\///
+* ///\\\
+*  //\\
+*   /\
 */
 
 public class Spikey {
