0f1420d07546e1720156c7cbd0052d3c8f2d11f2
baumsarah <48698640+baumsarah@users.noreply.github.com>
2020-05-13 11:02:19 -0400

    Create StarTriangle.java

---
+++ StarTriangle.java
@@ -0,0 +1,15 @@
+/* Write for loops to produce the following output:
+
+*
+**
+***
+****
+*****
+*/
+
+for(int i = 1; i < 6; i++) {
+    for(int j = 0; j < i; j++) {
+        System.out.print("*");
+    }
+    System.out.println();
+}
